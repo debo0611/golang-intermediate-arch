32170fd8fedea994316a5f33539e6fc6085a0ae4
Debojyoti <debojyoti.pramanick@gmail.com>
2022-01-17 01:31:23 +0530

    Base64 user:pass example

---
+++ Auth_basics/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type person struct {
+	First string
+}
+
+func main() {
+	// p1 := person{
+	// 	First: "Jenny",
+	// }
+
+	// p2 := person{
+	// 	First: "James",
+	// }
+
+	// xp := []person{p1, p2}
+
+	// bs, err := json.Marshal(xp)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println(string(bs))
+
+	// xp2 := []person{}
+	// err = json.Unmarshal(bs, &xp2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println("back into Go data structure", xp2)
+
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", foo)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	p1 := person{
+		First: "Jenny",
+	}
+	json.NewEncoder(w).Encode(p1)
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	pass
+}
--- main.go
+++ main.go
@@ -1,46 +1,10 @@
 package main
 
 import (
-	"net/http"
+	"encoding/base64"
+	"fmt"
 )
 
-type person struct {
-	First string
-}
-
 func main() {
-	// p1 := person{
-	// 	First: "Jenny",
-	// }
-
-	// p2 := person{
-	// 	First: "James",
-	// }
-
-	// xp := []person{p1, p2}
-
-	// bs, err := json.Marshal(xp)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-	// fmt.Println(string(bs))
-
-	// xp2 := []person{}
-	// err = json.Unmarshal(bs, &xp2)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-	// fmt.Println("back into Go data structure", xp2)
-
-	http.HandleFunc("/encode", foo)
-	http.HandleFunc("/decode", foo)
-	http.ListenAndServe(":8080", nil)
-}
-
-func foo(w http.ResponseWriter, r *http.Request) {
-	pass
-}
-
-func bar(w http.ResponseWriter, r *http.Request) {
-	pass
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
 }
